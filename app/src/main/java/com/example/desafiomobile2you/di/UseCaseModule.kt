912ec80bab460665bd31d91573b9ace9fae6dc69
Silas Miranda <developer.silas@gmail.com>
2021-09-14 20:56:39 -0300

    feat: creat usecase module

--- app/src/main/java/com/example/desafiomobile2you/di/UseCaseModule.kt
+++ app/src/main/java/com/example/desafiomobile2you/di/UseCaseModule.kt
@@ -1,9 +1,10 @@
 package com.example.desafiomobile2you.di
 
+
 import com.example.desafiomobile2you.domain.usecase.GetMovieDetailsUseCase
 import com.example.desafiomobile2you.domain.usecase.GetMovieDetailsUserCaseImpl
 import org.koin.dsl.module
 
 val useCaseModule =  module {
-    factory<GetMovieDetailsUseCase> { GetMovieDetailsUserCaseImpl(get()) }
+    factory <GetMovieDetailsUseCase> { GetMovieDetailsUserCaseImpl(get()) }
 }

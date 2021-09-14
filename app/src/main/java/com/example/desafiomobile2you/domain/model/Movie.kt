fc9d0e936b7459653d21b43e290ece187eefec9b
Silas Miranda <developer.silas@gmail.com>
2021-09-14 20:57:20 -0300

    feat: creat movie model

--- app/src/main/java/com/example/desafiomobile2you/domain/model/Movie.kt
+++ app/src/main/java/com/example/desafiomobile2you/domain/model/Movie.kt
@@ -1,6 +1,17 @@
 package com.example.desafiomobile2you.domain.model
 
-class Movie (
+import com.google.gson.annotations.SerializedName
 
+class Movie(
+
+    val id: Int,
+
+    @SerializedName("vote_count")
+    val voteCount: Int?,
+
+    @SerializedName("original_title")
+    val title: String?,
+
+    val popularity: Double?
 
 )

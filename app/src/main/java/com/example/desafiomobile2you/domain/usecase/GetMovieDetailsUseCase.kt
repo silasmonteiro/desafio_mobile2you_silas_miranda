2002fb60871c6279e88ebbbb8ddb6ca6dc5f91cb
Silas Miranda <developer.silas@gmail.com>
2021-09-14 20:56:07 -0300

    feat: creat moviedetails usecase

--- app/src/main/java/com/example/desafiomobile2you/domain/usecase/GetMovieDetailsUseCase.kt
+++ app/src/main/java/com/example/desafiomobile2you/domain/usecase/GetMovieDetailsUseCase.kt
@@ -1,9 +1,10 @@
 package com.example.desafiomobile2you.domain.usecase
 
 import com.example.desafiomobile2you.domain.model.Movie
+import com.example.desafiomobile2you.util.Result
 
 interface GetMovieDetailsUseCase {
 
-    fun getMovie(): Movie
+   suspend fun getMovie(movieId: String): Result<Movie>
 
 }
--- app/src/main/java/com/example/desafiomobile2you/domain/usecase/GetMovieDetailsUserCaseImpl.kt
+++ app/src/main/java/com/example/desafiomobile2you/domain/usecase/GetMovieDetailsUserCaseImpl.kt
@@ -1,11 +1,15 @@
 package com.example.desafiomobile2you.domain.usecase
 
 import com.example.desafiomobile2you.domain.model.Movie
+import com.example.desafiomobile2you.domain.repository.GetMovieDetailsRepository
+import com.example.desafiomobile2you.util.Result
 
-class GetMovieDetailsUserCaseImpl(private val getMovieRepository: GetMovieDetailsUseCase): GetMovieDetailsUseCase{
+class GetMovieDetailsUserCaseImpl(
+    private val getMovieDetailsRepository: GetMovieDetailsRepository
+    ): GetMovieDetailsUseCase{
 
-    override fun getMovie(): Movie {
-        TODO("Not yet implemented")
+    override suspend fun getMovie(movieId: String): Result<Movie> {
+       return getMovieDetailsRepository.getMovie(movieId = movieId)
     }
 
 }

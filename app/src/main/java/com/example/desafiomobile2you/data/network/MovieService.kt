c582e31f16b892620edcde5544846a5459850f7c
Silas Miranda <developer.silas@gmail.com>
2021-09-14 20:57:12 -0300

    feat: creat movieservice

--- app/src/main/java/com/example/desafiomobile2you/data/network/MovieService.kt
+++ app/src/main/java/com/example/desafiomobile2you/data/network/MovieService.kt
@@ -1,11 +1,18 @@
 package com.example.desafiomobile2you.data.network
 
 import com.example.desafiomobile2you.domain.model.Movie
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
+
+const val API_KEY = "b9a814f64ab6b4c56d504e1dbed36b8b"
 
 interface MovieService {
 
     @GET("movie/{movie_id}")
-    suspend fun getMovieDetails(@Path("movie_id") movieId: Int): Movie
+    suspend fun getMovieDetails(
+        @Path("movie_id") movieId: String,
+        @Query("api_key") api_key: String = API_KEY
+    ): Response<Movie>
 }

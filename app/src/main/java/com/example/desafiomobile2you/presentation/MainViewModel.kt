88b0e50436eec578c480a11cde9d45d344ec17f6
Silas Miranda <developer.silas@gmail.com>
2021-09-14 21:51:15 -0300

    feat: create liveData

--- app/src/main/java/com/example/desafiomobile2you/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/desafiomobile2you/presentation/MainViewModel.kt
@@ -1,9 +1,14 @@
 package com.example.desafiomobile2you.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.desafiomobile2you.base.BaseViewModel
+import com.example.desafiomobile2you.domain.model.Movie
 import com.example.desafiomobile2you.domain.usecase.GetMovieDetailsUseCase
 import com.example.desafiomobile2you.util.DispatcherProvider
+import com.example.desafiomobile2you.util.ResponseStatus
 import com.example.desafiomobile2you.util.Result
+import com.example.desafiomobile2you.util.ViewState
 import kotlinx.coroutines.launch
 
 
@@ -13,6 +18,8 @@ class MainViewModel(
     private val dispatcherProvider: DispatcherProvider
 ) : BaseViewModel(dispatcherProvider) {
 
+    private val _movieLiveData = MutableLiveData<ViewState<Movie, ResponseStatus>>()
+    val  movieLiveData: LiveData<ViewState<Movie, ResponseStatus>> = _movieLiveData
 
     fun getMovieDetails() {
 
@@ -20,11 +27,23 @@ class MainViewModel(
         scope.launch(dispatcherProvider.io) {
             when (val response = getMovieDetailsUseCase.getMovie(MOVIE_ID)) {
                 is Result.Success -> {
-                    response.data.let {
-
+                    response.data.let { movie ->
+                        _movieLiveData.postValue(
+                            ViewState(
+                                movie,
+                                ResponseStatus.SUCCESS
+                            )
+                        )
                     }
                 }
                 is Result.Failure -> {
+                    _movieLiveData.postValue(
+                        ViewState(
+                            null,
+                            ResponseStatus.ERROR,
+                            response.throwable
+                        )
+                    )
 
                 }
 

3d64a207b4164d6a0664f82ba6133c1bd6ce76a1
Silas Miranda <developer.silas@gmail.com>
2021-09-14 21:51:05 -0300

    feat: create liveData

--- app/src/main/java/com/example/desafiomobile2you/presentation/MainActivity.kt
+++ app/src/main/java/com/example/desafiomobile2you/presentation/MainActivity.kt
@@ -2,7 +2,12 @@ package com.example.desafiomobile2you.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import com.example.desafiomobile2you.R
+import com.example.desafiomobile2you.domain.model.Movie
+import com.example.desafiomobile2you.util.ResponseStatus
+import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +17,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setupObserverViewState(viewModel)
         viewModel.getMovieDetails()
+
+    }
+
+    private fun setupObserverViewState(viewModel: MainViewModel) {
+        viewModel.movieLiveData.observe(this, Observer { viewState ->
+            when (viewState.status) {
+
+                ResponseStatus.SUCCESS -> setupView(viewState.data)
+                ResponseStatus.ERROR -> showMessageError(viewState.error)
+            }
+        })
+    }
+
+    private fun showMessageError(error: Throwable?) {
+        error?.localizedMessage.let{
+            Toast.makeText(this,it,Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    private fun setupView(movie: Movie?) {
+
+        tv_titulo.text = movie?.title
+        tv_qtdlikes.text = movie?.voteCount.toString()
+        tv_qtdassistidos.text = movie?.popularity.toString()
 
     }
 }
